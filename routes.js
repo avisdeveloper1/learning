5490f6d87ad36681e7e3de1253a602464cd16e2d
CSkills <creativeskills@gmail.com>
2025-01-20 12:27:18 +0530

    Initial Upload
    Upload

---
+++ routes.js
@@ -0,0 +1,22 @@
+const requestHandler = (req, res) => {
+      const resData = {
+            message: "Hello, World!",
+            success: true,
+            data: [1, 2, 3, 4, 5, 6, 7],
+            url: req.url
+      };
+
+      res.setHeader('Content-Type', 'application/json');
+      res.write(JSON.stringify(resData));
+      res.end();
+}
+// module.exports = {
+//       handler : requestHandler,
+//       someText : "Test Data Handler"
+// }
+
+// module.exports.handler = requestHandler;
+// module.exports.someText = "Test Data Handler";
+
+exports.handler = requestHandler;
+exports.someText = "Test Data Handler";
